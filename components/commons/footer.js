9d54aca5e603a1dc671579a05867511b213aa283
Tamas Csonka <csnktms@gmail.com>
2019-08-07 11:11:44 +0200

    fix(footer): reduction of css, grouping logic
    Reduced the heaviness of some styles, merged style logic together

--- components/commons/footer.js
+++ components/commons/footer.js
@@ -1,10 +1,6 @@
 // translations
 import { Trans } from '@lingui/macro'
 import FooterStrips from '../decorations/footerStrips'
-import moment from 'moment'
-// Icons
-import { FaGithub, FaTwitter, FaWhatsapp } from 'react-icons/fa';
-import { MdEmail } from 'react-icons/md';
 
 class Footer extends React.Component {
   render() {
@@ -12,33 +8,7 @@ class Footer extends React.Component {
 
     return (
       <footer>
-        <h5>
-          {`© ${moment().year()} ${show.name}`} <br/>
-        </h5>
-        <div className="social-box">
-          <span className="subtitle2">
-            <a href={`mailto:${show.email}`}>
-              <MdEmail />
-            </a>
-          </span>
-          <span className="subtitle2">
-            <a href={show.github} target="_blank">
-              <FaGithub />
-            </a>
-          </span>
-          <span className="subtitle2">
-            <a href={show.twitter} target="_blank">
-              <FaTwitter />
-            </a>
-          </span>
-          <span className="subtitle2">
-            <a href={`tel:${show.phone}`} target="_blank">
-              <FaWhatsapp />
-            </a>
-          </span>
-        </div>
-        
-        <FooterStrips />
+        <FooterStrips show={show}/>
       </footer>
     )
   }
@@ -51,4 +21,3 @@ export default Footer
 //   <Trans id="footer.mail">email me!</Trans>
 // </a>
 // </p>
-
--- components/decorations/footerStrips.js
+++ components/decorations/footerStrips.js
@@ -1,11 +1,42 @@
-const FooterStrips = () => (
+import moment from 'moment'
+import { FaGithub, FaTwitter, FaWhatsapp } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
+
+const FooterStrips = ({ show }) => (
   <div className="footer-stripe-box">
-    <div className="footer-stripe-1"></div>
-    <div className="footer-stripe-2"></div>
-    <div className="footer-stripe-3"></div>
-    <div className="footer-stripe-4"></div>
-    <div className="footer-stripe-5"></div>
-    <div className="footer-stripe-6"></div>
+    <div className="footer-stripe footer-stripe-1"></div>
+    <div className="footer-stripe footer-stripe-2"></div>
+    <div className="footer-stripe footer-stripe-3"></div>
+    <div className="footer-stripe footer-stripe-4"></div>
+    <div className="footer-stripe footer-stripe-5"></div>
+    <div className="footer-stripe footer-stripe-6"></div>
+    <div className="social-wrapper">
+        <h5>
+          {`© ${moment().year()} ${show.name}`} <br/>
+        </h5>
+        <div className="social-box">
+          <span className="subtitle2">
+            <a href={`mailto:${show.email}`}>
+              <MdEmail />
+            </a>
+          </span>
+          <span className="subtitle2">
+            <a href={show.github} target="_blank">
+              <FaGithub />
+            </a>
+          </span>
+          <span className="subtitle2">
+            <a href={show.twitter} target="_blank">
+              <FaTwitter />
+            </a>
+          </span>
+          <span className="subtitle2">
+            <a href={`tel:${show.phone}`} target="_blank">
+              <FaWhatsapp />
+            </a>
+          </span>
+        </div>
+      </div>
   </div>
 )
 
