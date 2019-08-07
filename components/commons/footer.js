c57d06048847fb71ce1222a73f76229a48ae47b7
Tamas Csonka <csnktms@gmail.com>
2019-08-07 11:45:29 +0200

    fix(footer): move social-box to footer
    maintain the presentational logic of strips

--- components/commons/footer.js
+++ components/commons/footer.js
@@ -1,5 +1,8 @@
 // translations
 import { Trans } from '@lingui/macro'
+import moment from 'moment'
+import { FaGithub, FaTwitter, FaWhatsapp } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 import FooterStrips from '../decorations/footerStrips'
 
 class Footer extends React.Component {
@@ -8,7 +11,34 @@ class Footer extends React.Component {
 
     return (
       <footer>
-        <FooterStrips show={show}/>
+        <div className="social-wrapper">
+        <h5>
+          {`© ${moment().year()} ${show.name}`} <br/>
+        </h5>
+        <div className="social-box">
+          <span className="subtitle2">
+            <a href={`mailto:${show.email}`}>
+              <MdEmail />
+            </a>
+          </span>
+          <span className="subtitle2">
+            <a href={show.github} target="_blank">
+              <FaGithub />
+            </a>
+          </span>
+          <span className="subtitle2">
+            <a href={show.twitter} target="_blank">
+              <FaTwitter />
+            </a>
+          </span>
+          <span className="subtitle2">
+            <a href={`tel:${show.phone}`} target="_blank">
+              <FaWhatsapp />
+            </a>
+          </span>
+        </div>
+      </div>
+        <FooterStrips />
       </footer>
     )
   }
--- components/decorations/footerStrips.js
+++ components/decorations/footerStrips.js
@@ -1,7 +1,3 @@
-import moment from 'moment'
-import { FaGithub, FaTwitter, FaWhatsapp } from 'react-icons/fa';
-import { MdEmail } from 'react-icons/md';
-
 const FooterStrips = ({ show }) => (
   <div className="footer-stripe-box">
     <div className="footer-stripe footer-stripe-1"></div>
@@ -10,33 +6,6 @@ const FooterStrips = ({ show }) => (
     <div className="footer-stripe footer-stripe-4"></div>
     <div className="footer-stripe footer-stripe-5"></div>
     <div className="footer-stripe footer-stripe-6"></div>
-    <div className="social-wrapper">
-        <h5>
-          {`© ${moment().year()} ${show.name}`} <br/>
-        </h5>
-        <div className="social-box">
-          <span className="subtitle2">
-            <a href={`mailto:${show.email}`}>
-              <MdEmail />
-            </a>
-          </span>
-          <span className="subtitle2">
-            <a href={show.github} target="_blank">
-              <FaGithub />
-            </a>
-          </span>
-          <span className="subtitle2">
-            <a href={show.twitter} target="_blank">
-              <FaTwitter />
-            </a>
-          </span>
-          <span className="subtitle2">
-            <a href={`tel:${show.phone}`} target="_blank">
-              <FaWhatsapp />
-            </a>
-          </span>
-        </div>
-      </div>
   </div>
 )
 
