2d947a020140333b4d5746138ee5278efea7ee03
Alonso Lamas <lamas.alonso@gmail.com>
2019-09-17 19:19:52 +0200

    Add custom error page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const withSass = require('@zeit/next-sass')
 
 module.exports = withSass({
+  // cssModules: true,
+  // cssLoaderOptions: {
+  //   importLoaders: 1,
+  //   localIdentName: '[local]___[hash:base64:5]'
+  // },
   webpack: (config, options) => {
     config.module.rules.push({
       test: /\.po/,
